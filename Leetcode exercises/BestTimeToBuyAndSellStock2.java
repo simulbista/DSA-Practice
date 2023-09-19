640762cb2396462ed83790a68c145ecfbc1f1c37
Simul <simulbista@gmail.com>
2023-09-19 16:35:30 -0400

    380. Insert Delete GetRandom O(1)  (Medium) problem solved

--- Leetcode exercises/BestTimeToBuyAndSellStock2.java
+++ Leetcode exercises/BestTimeToBuyAndSellStock2.java
@@ -1,3 +1,14 @@
+//122. Best Time to Buy and Sell Stock II (medium)
+
+// on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+//Total profit is 4.
+//Example 3:
+//
+//Input: prices = [7,6,4,3,1]
+//Output: 0
+//Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+
+
 public class BestTimeToBuyAndSellStock2 {
     public static int maxProfit(int[] prices){
         int maxP=0;
